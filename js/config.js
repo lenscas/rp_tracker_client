550c24ba03e89a90687a3cf57b0f1ca97d6de81d
lenscas <lenscas@lenscas-desktop>
2017-07-15 12:39:10 +0200

    first commit

---
+++ js/config.js
@@ -0,0 +1,5 @@
+const conf = {}
+conf.api      = "http://localhost/tracker/index.php/api/"; "http://localhost/rpInterface";
+conf.prot     = "http://"
+conf.base_url = conf.prot+"localhost/tracker_client/" 
+conf.pages    = conf.base_url +"pages/"
---
+++ js/routes.js
@@ -0,0 +1,4 @@
+routes = {
+	"index" : ["basic/index","basicIndex"],
+	"login" : ["users/login","usersLogin"]
+}
