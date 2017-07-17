b60a7fd6840ef3a90ced6c02a3488a9c4e84b08d
lenscas <lenscas@gmail.com>
2017-07-17 22:47:01 +0200

    Started work on the interface Menu can be collapsed, event to do so just needs to be fired. For the rest its a basic mockup, links go nowhere and you can't login

--- js/config.js
+++ js/config.js
@@ -3,6 +3,7 @@ conf.api      = "http://localhost/tracker/index.php/api/";
 conf.prot     = "http://";
 conf.base_url = conf.prot+"localhost/tracker_client/" ;
 conf.js       = conf.base_url + "js/";
+conf.css      = conf.base_url + "css/";
 conf.pages    = conf.base_url + "pages/";
 conf.debug    = true;
 $.ajaxSetup({ cache: false });
